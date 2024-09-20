28446b1f53aadab85eeecceef0869c0e13814773
Mugabepro-1 <mugabepromesse@gmail.com>
2024-09-20 13:50:27 +0200

    Removed the client side confusion

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.get('/mp4download', async(req,res)=>{
 })
 
 
-const port = process.env.port || 3000;
+const port = 3000;
 app.listen(port, ()=>{
     console.log(`App running on port ${port}`);
 })
